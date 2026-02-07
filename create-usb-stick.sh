6df184a2f6b128eee551fe2b9d7ab321abba4ad0
Henry den Hengst <henrydenhengst@gmail.com>
2026-02-07 13:41:20 +0100

    Update create-usb-stick.sh
    version 2

--- create-usb-stick.sh
+++ create-usb-stick.sh
@@ -1,1 +1,4 @@
-sudo dd if=debian-12.5.0-amd64-netinst.iso of=/dev/sdX bs=4M status=progress conv=fsync
+wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-13.0.0-amd64-netinst.iso
+
+sudo dd if=debian-13.0.0-amd64-netinst.iso of=/
+dev/sdX bs=4M status=progress conv=fsync
